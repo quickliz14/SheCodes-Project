c6ff6bba2d6742018f8b5c87594a64ea0d59d489
quickliz14 <114953604+quickliz14@users.noreply.github.com>
2022-10-04 18:39:40 -0400

    Resaved Project

---
+++ Wireframe/src/index.js
@@ -0,0 +1,62 @@
+//Assignment 1
+let now = new Date();
+let currentTime = document.querySelector("#time");
+
+let hour = now.getHours();
+let minutes = now.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+currentTime.innerHTML = `${day} ${hour}:${minutes}`;
+
+///Assignment 2
+function changeCity(event) {
+  event.preventDefault();
+  let h1 = document.querySelector("h1");
+  let searchForm = document.querySelector(".form-control");
+  h1.innerHTML = `${searchForm.value}`;
+}
+
+let searchButton = document.querySelector("form");
+searchButton.addEventListener("submit", changeCity);
+
+//week 5 homework
+function showWeather(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weather-desc").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed * 3.6
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "1852aed5ea516d2b62e398fa77506e7c";
+  let unit = "imperial";
+  let apiEndPoint = "https://api.openweathermap.org/data";
+  let apiUrl = `${apiEndPoint}/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#input-city-name").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+searchCity("Los Angeles");
