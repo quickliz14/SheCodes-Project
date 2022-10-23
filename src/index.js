f6e08d993fb21f88df3c0f77b91d1189f663b20b
quickliz14 <114953604+quickliz14@users.noreply.github.com>
2022-10-22 23:21:23 -0400

    Lesson 4
    Lesson 4 JS updated

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ let day = days[now.getDay()];
 currentTime.innerHTML = `${day} ${hour}:${minutes}`;
 
 //Forecast
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -38,7 +38,7 @@ function displayForecast() {
 <div class="col-2">
 <div class="weather-forecast-date">${day}</div>
 <img
-src="http://openweathermap.org/img/wn/50d@2x.png"
+src="https://openweathermap.org/img/wn/50d@2x.png"
 alt=""
 width="42"
 />
@@ -52,6 +52,12 @@ width="42"
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "1852aed5ea516d2b62e398fa77506e7c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 ///New City
@@ -91,9 +97,11 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
-//Weather API
+//Weather API Call
 function searchCity(city) {
   let apiKey = "1852aed5ea516d2b62e398fa77506e7c";
   let unit = "metric";
@@ -139,4 +147,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 searchCity("Los Angeles");
-displayForecast();
