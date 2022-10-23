2716284c2d42178ec5ef7024d97c40e28ffbfb88
quickliz14 <114953604+quickliz14@users.noreply.github.com>
2022-10-23 00:03:50 -0400

    Update index.js
    trying the she codes api key

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ searchButton.addEventListener("submit", changeCity);
 
 //Forecast Function API
 function getForecast(coordinates) {
-  let apiKey = "1852aed5ea516d2b62e398fa77506e7c";
+  let apiKey = "b2d9fa1f2b35557e4615dd5fab218834";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
   console.log(apiUrl);
 
