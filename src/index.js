bec8f0f24be8fad2b2e9b8edb9bc7272b3300ff5
quickliz14 <114953604+quickliz14@users.noreply.github.com>
2022-10-22 23:23:47 -0400

    HTTPS Updates
    Https link updates

--- src/index.js
+++ src/index.js
@@ -94,7 +94,7 @@ function showWeather(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
